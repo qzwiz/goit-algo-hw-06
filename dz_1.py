030b1aadb271a903133a519d7f1a0b5b69c35ed0
qzwiz <naum385385@gmail.com>
2025-02-20 19:30:03 +0200

    v2

--- dz_1.py
+++ dz_1.py
@@ -13,8 +13,10 @@ class Name(Field):
 
 class Phone(Field):
     def __init__(self, value):
-           if  len(value) == 10 and value.isdigit():
-               self.value = value
+           if not (len(value) == 10 and value.isdigit()):
+               raise ValueError("нужно 10 цифр")
+           super().__init__(value)
+            
 
 class Record:
     def __init__(self, name):
@@ -25,15 +27,16 @@ class Record:
         self.phones.append(Phone(phone))
     
     def remove_phone(self, phone):
-        self.phones = [i for i in self.phones if i.value != phone]
+        phone_obj = self.find_phone(phone)
+        if phone_obj:
+            self.phones.remove(phone_obj)
     
-
     def edit_phone(self, old_phone, new_phone):
-        for phone in self.phones:
-            if phone.value == old_phone:
-                phone.value = new_phone
-                return
-        raise ValueError("нету такого телефона")
+        phone_obj = self.find_phone(old_phone)
+        if not phone_obj:
+            raise ValueError("Phone number not found.")
+        self.remove_phone(old_phone)
+        self.add_phone(new_phone)
 
     def find_phone(self, phone):
         for p in self.phones:
@@ -41,23 +44,22 @@ class Record:
                 return p
         return None
 
-
-    # реалізація класу
-
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
+    
     def find(self, name):
-        return self.data[name]
+        return self.data.get(name, None)
+    
     def delete(self, name):
-        del self.data[name]
+        if name in self.data:
+            del self.data[name]
+    
     def __str__(self):
-        return "\n".join(str(record) for record in self.data.values())        
-
-
+        return "\n".join(str(record) for record in self.data.values())
 
 # Створення нової адресної книги
 book = AddressBook()
@@ -90,5 +92,3 @@ print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
 
     # Видалення запису Jane
 book.delete("Jane")
-
-AddressBook
